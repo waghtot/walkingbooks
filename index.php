093c9f791f073780e9957134cb6c79a908d35c49
waghtot <waghtot@gmail.com>
2024-01-28 16:16:33 +0000

    update

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/app/view/css/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="/app/view/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/app/view/css/test.css" rel="stylesheet">
+    <title>wb</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="login" class="form-label">Login:</label>
+                                <input type="text" name="login" class="form-control">
+                            </div>
+                            <div class="form-group mt-2">
+                                <label for="password" class="form-label">Password:</label>
+                                <input type="text" name="password" class="form-control">
+                            </div>
+                            <div class="form-group mt-3">
+                                <button type="submit" class="btn btn-success btn-small">Log in</button>
+                                <button type="button" class="btn btn-dark btn-small">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
